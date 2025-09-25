ea34ed81c65627a2e7f170cbb701a5ab1c196c74
Ritik Gupta <ritikgupta26052002@gmail.com>
2025-09-25 09:21:21 +0530

    Initial commit - Simple Todo Chrome Extension

---
+++ public/background.js
@@ -0,0 +1,1 @@
+console.log("Todo extension background running...");
--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+/* global chrome */
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    chrome.storage.local.get(["todos"], (res) => {
+      if (res.todos) setTodos(res.todos);
+    });
+  }, []);
+
+  const addTodo = () => {
+    if (!input) return;
+    const newTodos = [...todos, input];
+    setTodos(newTodos);
+    chrome.storage.local.set({ todos: newTodos });
+    setInput("");
+  };
+
+  const deleteTodo = (i) => {
+    const newTodos = todos.filter((_, index) => index !== i);
+    setTodos(newTodos);
+    chrome.storage.local.set({ todos: newTodos });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: 250, padding: 10 }}>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add todo"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        {todos.map((todo, i) => (
+          <li key={i}>
+            {todo} <button onClick={() => deleteTodo(i)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
